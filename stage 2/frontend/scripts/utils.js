64dd1f421c63b78a2bbe0152a5d334c7f249e5dd
Igor Elizaroff <igor.elizaroff@yandex.ru>
2022-10-26 08:38:00 +0300

    Revert "Added backend"
    This reverts commit b71800ecaaf701ae3ce430a8871816391d485487.

--- stage 2/frontend/scripts/utils.js
+++
@@ -1,5 +0,0 @@
-export const parsePercent = (str) => {
-    if (str.indexOf("%") === -1) return ["", str]       // если нет "%" - возврат ["", исходная строка]
-
-    return [str.substring(0, str.indexOf("%") + 1), str.substring(str.indexOf("%") + 2, str.length)]
-}
